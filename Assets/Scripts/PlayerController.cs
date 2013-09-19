590c5eab83ac8daca994a3645c06ef87da001955
Edward Rowe <tehgiver@gmail.com>
2013-09-19 16:04:33 -0400

    Added script to find nearest tile to Reticule.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,17 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	
 	public float movespeed;
+	public GameObject reticulePrefab;
 	
+	public GUIText debugText;
+	
+	private GameObject reticule;
 	private float turnSmoothing = 90f;
+	
+	void Start()
+	{
+		SpawnReticule();
+	}
 	
 	void FixedUpdate () {
 		float movementHorizontal = Input.GetAxis("Horizontal");
@@ -17,6 +26,29 @@ public class PlayerController : MonoBehaviour {
 		}
 		
 		Move(movementHorizontal, movementVertical);
+		
+		GameObject tile = GetActionTile();
+		if ( tile != null)
+		{
+			debugText.text = tile.name;
+		}
+		else
+		{
+			debugText.text = "";
+		}
+	}
+	
+	// Spawn a reticule from the Player using the assigned Prefab
+	private void SpawnReticule()
+	{
+		// Configure the initial offset for the reticule
+		float xOffset = 0.0f;
+		float yOffset = -0.75f;
+		float zOffset = 1f;
+		Vector3 offset = new Vector3(xOffset, yOffset, zOffset);
+		
+		reticule = (GameObject) Instantiate (reticulePrefab, transform.position + offset, Quaternion.identity);
+		reticule.transform.parent = transform;
 	}
 	
 	void Move(float horizontal, float vertical)
@@ -34,5 +66,27 @@ public class PlayerController : MonoBehaviour {
 		Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, desiredRotation, turnSmoothing * Time.deltaTime);
         
 		rigidbody.MoveRotation(newRotation);
+	}
+	
+	void TryAction()
+	{
+		
+	}
+	
+	GameObject GetActionTile()
+	{
+		GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+		GameObject closestTile = null;
+		float bestD = Mathf.Infinity;
+		foreach( GameObject tile in tiles)
+		{
+			float dToTileSquared = (tile.transform.position - reticule.transform.position).sqrMagnitude;
+			if (dToTileSquared < bestD)
+			{
+				bestD = dToTileSquared;
+				closestTile = tile;
+			}
+		}
+		return closestTile;
 	}
 }
