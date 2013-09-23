9652375fe327e1dd71ddb9c53429ea4e208d931f
LucasRowe <lucas.rowe@gmail.com>
2013-09-23 11:39:51 -0400

    Very basic inventory added. Cleanup of ingored files.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+    private int numRadishes;
+    
+	void Start () {
+        numRadishes = 0;
+	}
+	
+	// Stub method for testing 
+	void Update ()
+	{
+		if(Input.GetKeyDown("p"))
+		{
+			addRadish();
+		}
+	}
+	
+    public void addRadish()
+    {
+        numRadishes++;
+    }
+
+    public void removeRadish()
+    {
+        numRadishes--;
+    }
+
+    public int getRadishCount()
+    {
+        return numRadishes;
+    }
+}
