ae7bec7bb3d8742dec52f78bf783b78fc8f3b9a6
Edward Rowe <tehgiver@gmail.com>
2013-09-24 12:13:19 -0400

    Added Sunlight and WorldTime to allow for progression of time.

---
+++ Assets/Scripts/Sunlight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sunlight : MonoBehaviour {
+	
+	void Start () {
+		// Initialize the position for the sun
+		transform.rotation = Quaternion.identity;
+		transform.Rotate (new Vector3(60, 300, 30), Space.World);
+	}
+	
+	/*
+	 * Sets the sun position based on the supplied hour.
+	*/
+	public void SetSunToTimeOfDay( float hour )
+	{
+		float MAX_INTENSITY = 0.5f;
+		float highNoon = WorldTime.GAMEHOURS_PER_GAMEDAY / 2;
+		
+		Light theLight = GetComponent<Light>();
+		
+		// Sun goes down after high noon
+		if(hour > highNoon)
+		{
+			theLight.light.intensity = MAX_INTENSITY * (1 - ((hour - highNoon) / highNoon));
+		}
+		else
+		{
+			theLight.light.intensity = MAX_INTENSITY * (hour / highNoon);
+		}
+	}
+}
---
+++ Assets/Scripts/WorldTime.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldTime : MonoBehaviour {
+	
+	public GUIText timeGUI;
+	public GUIText hourGUI;
+	public GUIText minuteGUI;
+	public GUIText dayGUI;
+	
+	public Light sun;
+	
+	private int day;
+	private float dayTimeInGame;
+		
+	public static int SECONDS_PER_GAMEDAY = 240;
+	
+	public static int GAMEHOURS_PER_GAMEDAY = 24;
+	public static int GAMEMINUTES_PER_GAMEHOUR = 60;
+	public static int GAMESECONDS_PER_GAMEMINUTE = 60;
+
+	private static float SECONDS_PER_GAMEHOUR = SECONDS_PER_GAMEDAY / GAMEHOURS_PER_GAMEDAY;
+	private static float SECONDS_PER_GAMEMINUTE = SECONDS_PER_GAMEHOUR / GAMEMINUTES_PER_GAMEHOUR;
+	private static float SECONDS_PER_GAMESECOND = SECONDS_PER_GAMEMINUTE / GAMESECONDS_PER_GAMEMINUTE;
+	
+	private static float DAWN = 6.0f;
+	
+	void Start()
+	{
+		if( Mathf.Approximately(SECONDS_PER_GAMESECOND, 0.0f) )
+		{
+			Debug.LogError("Seconds per Game Second is too small for a float! Adjust Game hours and minutes until it fits.");
+			SECONDS_PER_GAMESECOND = 0.02f;
+		}
+		day = 0;
+		SetGameTimeToHour (DAWN);
+	}
+	
+	void Update () {
+		IncrementTimeInGame(Time.deltaTime);
+		
+		timeGUI.text = "Total Time: " + dayTimeInGame;
+		minuteGUI.text = "Minute: " + GetDayTimeInMinutes() % GAMEMINUTES_PER_GAMEHOUR;
+		hourGUI.text = "Hour: " + GetDayTimeInHours();
+		dayGUI.text = "Day: " + day;
+	}
+	
+	/*
+	 * Increment the game time based on the supplied delta timestep
+	*/
+	private void IncrementTimeInGame(float deltaTime)
+	{
+		SetGameTime (dayTimeInGame + ConvertSecondsToGameSeconds(deltaTime));
+		
+		// When we step over the time per day, snap to the next day.
+		float gameSecondsInDay = ( GAMESECONDS_PER_GAMEMINUTE * GAMEMINUTES_PER_GAMEHOUR * GAMEHOURS_PER_GAMEDAY  );
+		if(dayTimeInGame > gameSecondsInDay)
+		{
+			GoToNextDay ();
+		}
+	}
+	
+	/*
+	 * Snaps the enviornment and timers to the next day
+	*/
+	public void GoToNextDay()
+	{
+		day++;
+		SetGameTimeToHour (DAWN);
+	}
+	
+	// Set the game time to the specified total second
+	public void SetGameTime(float seconds)
+	{
+		dayTimeInGame = seconds;
+		
+		Sunlight sunScript = sun.GetComponent<Sunlight>();
+		sunScript.SetSunToTimeOfDay(GetDayTimeInHours());
+	}
+	
+	// Set the game time to the specified hour
+	public void SetGameTimeToHour( float hours)
+	{
+		SetGameTime (hours * GAMESECONDS_PER_GAMEMINUTE * GAMEMINUTES_PER_GAMEHOUR);
+	}	
+	
+	// Returns the total Game Time passed in Game Minutes
+	public float GetDayTimeInMinutes()
+	{
+		return ConvertSecondsToGameMinutes (dayTimeInGame);
+	}
+	
+	// Returns the total time for the day in Game Hours
+	public float GetDayTimeInHours()
+	{
+		return ConvertSecondsToGameHours (dayTimeInGame);
+	}
+	
+	// Converts specified Seconds to Game Seconds
+	private float ConvertSecondsToGameSeconds( float seconds )
+	{
+		return seconds / SECONDS_PER_GAMESECOND;
+	}
+	
+	// Converts specified Seconds to Game Minutes
+	private float ConvertSecondsToGameMinutes( float seconds )
+	{
+		return seconds / GAMESECONDS_PER_GAMEMINUTE;
+	}
+	
+	// Converts specified Seconds to Game Hours
+	private float ConvertSecondsToGameHours( float seconds )
+	{
+		return ConvertSecondsToGameMinutes(seconds) / GAMEMINUTES_PER_GAMEHOUR;
+	}
+}
